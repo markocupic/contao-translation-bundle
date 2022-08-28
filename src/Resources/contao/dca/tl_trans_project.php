c84bf8ce3e37787d92c4c9b058417306db893634
Marko Cupic <m.cupic@gmx.ch>
2022-08-28 11:09:47 +0200

    Fix undefined array key issues

--- src/Resources/contao/dca/tl_trans_project.php
+++ src/Resources/contao/dca/tl_trans_project.php
@@ -77,7 +77,7 @@ $GLOBALS['TL_DCA']['tl_trans_project'] = [
             'delete'     => [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'       => [
                 'href'       => 'act=show',
--- src/Resources/contao/dca/tl_trans_resource.php
+++ src/Resources/contao/dca/tl_trans_resource.php
@@ -60,7 +60,7 @@ $GLOBALS['TL_DCA']['tl_trans_resource'] = [
             'delete'     => [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'       => [
                 'href'       => 'act=show',
